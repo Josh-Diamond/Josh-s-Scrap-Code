6c9907019432ac5da5f0432f7fad2dc190d8110c
Josh Diamond <diamondroad91@gmail.com>
2019-11-04 15:18:28 -0700

    org page n styles

--- client/src/components/OrgCard.jsx
+++ client/src/components/OrgCard.jsx
@@ -6,7 +6,7 @@ import OrgCardBottomBar from './OrgCardBottomBar'
 
 export default function OrgCard() {
     return (
-        <div className={css({ borderRadius: '5px', width: '400px', margin: '25px auto'})}>
+        <div className={css({ borderRadius: '5px', width: '400px', margin: '2% 15%'})}>
                     <OrgCardTopBar />
                     <OrgCardTable />
                     <OrgCardBottomBar />
--- client/src/views/Admin.jsx
+++ client/src/views/Admin.jsx
@@ -7,9 +7,23 @@ export default function Admin() {
     return (
         <div className={css({ display: 'flex'})}>
             <Nav />
-            <div className={css({ margin: '0 5%', width: '100%', display: 'flex', alignItems: 'flex-start'})}>
-                <h2 className={css({ fontWeight: '400' })}>WellDone Sensors</h2>
+            <div className={css({ width: '100%' })}>
+                {/* top part */}
+            <div className={css({  width: '100%', display: 'flex', alignItems: 'flex-start'})}>
+                <h2 className={css({ margin: '2% 5%', fontWeight: '400' })}>WellDone Sensors</h2>
                 <OrgCard />
+            </div>
+                {/* end top part */}
+                {/* Middle Line */}
+            <div className={css({ borderBottom: '1px solid #082B84', width: '100%'})}/>
+                {/* Middle Line End */}
+
+                {/* Bottom Part */}
+                <div className={css({ width: '100%', display: 'flex', alignItems: 'center'})}>
+                <h2 className={css({ margin: '2% 5%', fontWeight: '400' })}>Organizations</h2>
+                <button className={css({ margin: '25px 100px', cursor: 'pointer', border: 'none', width: '210px', height: '42px', backgroundColor: '#027EF3', color: 'white', fontSize: '1rem' })}>Create New Organization</button>
+            </div>
+                {/* End Bottom Part */}
             </div>
         </div>
     )
