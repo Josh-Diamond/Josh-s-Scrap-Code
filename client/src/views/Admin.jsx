a895d6c6fd3291ca124f902558b8db178e9a9acf
Josh Diamond <diamondroad91@gmail.com>
2019-11-04 11:09:38 -0700

    org card styles

--- client/src/views/Admin.jsx
+++ client/src/views/Admin.jsx
@@ -25,10 +25,10 @@ export default function Admin() {
                     {/* Table  */}
                     <table className={css({ width: '100%', borderCollapse: 'collapse'})}>
                         <tr className={css({ backgroundColor: '#CEE7FF'})}>
-                            <th className={css({ textAlign: 'center'})}>Sensor ID</th>
-                            <th className={css({ textAlign: 'center'})}>Date Added</th>
-                            <th className={css({ textAlign: 'center'})}>Status</th>
-                            <th className={css({ textAlign: 'center'})}>Remove</th>
+                            <th className={css({ textAlign: 'center', color: '#464646', fontWeight: '400'})}>Sensor ID</th>
+                            <th className={css({ textAlign: 'center', color: '#464646', fontWeight: '400'})}>Date Added</th>
+                            <th className={css({ textAlign: 'center', color: '#464646', fontWeight: '400'})}>Status</th>
+                            <th className={css({ textAlign: 'center', color: 'black', fontWeight: '400'})}>Remove</th>
                         </tr>
                         <tr className={css({ borderLeft: '1px solid #082B84', borderRight: '1px solid #082B84'})}>
                             <td className={css({ textAlign: 'left', paddingLeft: '9px', borderBottom: '1px solid #C7C9CF'})}>4716</td>
@@ -40,43 +40,43 @@ export default function Admin() {
                             <td className={css({ textAlign: 'left', paddingLeft: '9px', borderBottom: '1px solid #C7C9CF'})}>4721</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>09/26/2019</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>94</td>
-                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input type='checkbox' /></td>
+                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input className={css({ cursor: 'pointer'})} type='checkbox' /></td>
                         </tr>
                         <tr className={css({ borderLeft: '1px solid #082B84', borderRight: '1px solid #082B84'})}>
                             <td className={css({ textAlign: 'left', paddingLeft: '9px', borderBottom: '1px solid #C7C9CF'})}>4721</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>09/26/2019</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>94</td>
-                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input type='checkbox' /></td>
+                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input className={css({ cursor: 'pointer'})} type='checkbox' /></td>
                         </tr>
                         <tr className={css({ borderLeft: '1px solid #082B84', borderRight: '1px solid #082B84'})}>
                             <td className={css({ textAlign: 'left', paddingLeft: '9px', borderBottom: '1px solid #C7C9CF'})}>4721</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>09/26/2019</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>94</td>
-                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input type='checkbox' /></td>
+                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input className={css({ cursor: 'pointer'})} type='checkbox' /></td>
                         </tr>
                         <tr className={css({ borderLeft: '1px solid #082B84', borderRight: '1px solid #082B84'})}>
                             <td className={css({ textAlign: 'left', paddingLeft: '9px', borderBottom: '1px solid #C7C9CF'})}>4721</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>09/26/2019</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>94</td>
-                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input type='checkbox' /></td>
+                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input className={css({ cursor: 'pointer'})} type='checkbox' /></td>
                         </tr>
                         <tr className={css({ borderLeft: '1px solid #082B84', borderRight: '1px solid #082B84'})}>
                             <td className={css({ textAlign: 'left', paddingLeft: '9px', borderBottom: '1px solid #C7C9CF'})}>4721</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>09/26/2019</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>94</td>
-                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input type='checkbox' /></td>
+                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input className={css({ cursor: 'pointer'})} type='checkbox' /></td>
                         </tr>
                         <tr className={css({ borderLeft: '1px solid #082B84', borderRight: '1px solid #082B84'})}>
                             <td className={css({ textAlign: 'left', paddingLeft: '9px', borderBottom: '1px solid #C7C9CF'})}>4721</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>09/26/2019</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>94</td>
-                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input type='checkbox' /></td>
+                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input className={css({ cursor: 'pointer'})} type='checkbox' /></td>
                         </tr>
                         <tr className={css({ borderLeft: '1px solid #082B84', borderRight: '1px solid #082B84'})}>
                             <td className={css({ textAlign: 'left', paddingLeft: '9px', borderBottom: '1px solid #C7C9CF'})}>4721</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>09/26/2019</td>
                             <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}>94</td>
-                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input type='checkbox' /></td>
+                            <td className={css({ textAlign: 'center', borderBottom: '1px solid #C7C9CF'})}><input className={css({ cursor: 'pointer'})} type='checkbox' /></td>
                         </tr>
                         </table>
                     {/* Table Ends */}
